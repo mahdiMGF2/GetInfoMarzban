fa0ad88887217293b2512aeab6af3bfc7369677a
MahdiGholipour <mahdigholipour@outlook.com>
2023-02-24 01:52:13 +0330

    add username

---
+++ config.php
@@ -0,0 +1,4 @@
+<?php
+$token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJtYWhkaTIwMDMiLCJhY2Nlc3MiOiJzdWRvIiwiZXhwIjoxNjc3MjcyMDE1fQ.lYBx64KS7QwPD8H_DWcA5o0F57UVmIWsqbfL1OoOKr8';//api توکن
+$ip_port = 'http://91.107.185.91:8880'; // آدرس آیپی همراه با پورت 
+?>
---
+++ user.php
@@ -0,0 +1,176 @@
+<?php
+include('config.php');
+//username get
+$usernameac = $_GET['usernames'];
+$url = $ip_port.'/api/user/'.$usernameac;
+$header_value = 'Bearer ';
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_HTTPGET, true);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Accept: application/json',
+    'Authorization: ' . $header_value . $token));
+
+$output = curl_exec($ch);
+curl_close($ch);
+$Data = json_decode($output, true);
+
+
+//username
+$username = $Data['username'];
+
+
+//status
+$status = $Data['status'];
+switch ($status) {
+    case 'active':
+        $color_active = "#47bf2f";
+        $text_status = "فعال";
+        break;
+    case 'limited':
+        $color_active = "#bf2e2e";
+        $text_status = "پایان حجم";
+        break;
+
+    default:
+        $color_active = "#828282";
+        $text_status = "نامشخص";
+        break;
+}
+
+
+
+//TOTAL TRAFFIC
+$dataLimit = $Data['data_limit'];
+$LastTraffic = round($dataLimit / 1073741824, 2) . "GB";
+if ($LastTraffic < 1) {
+    $LastTraffic = round($dataLimit / 1073741824, 2) * 1000 . "MB";
+}
+if ($LastTraffic == 0) {
+    $LastTraffic = "نامحدود";
+    $RemainingVolume = "نامحدود";
+}
+
+//used_traffic
+$usedTraffic = $Data['used_traffic'];
+$usedTrafficGb = round($usedTraffic / 1073741824, 2) . "GB";
+if (round($usedTraffic / 1073741824, 2) < 1) {
+    $usedTrafficGb = round($usedTraffic / 1073741824, 2) * 1000 . "MB";
+}
+if (round($usedTraffic / 1073741824, 2) == 0) {
+    $usedTrafficGb = "مصرف نشده";
+}
+
+
+
+// remaining volume
+if (round($dataLimit / 1073741824, 2) != 0) {
+    $min = round($dataLimit / 1073741824, 2) - round($usedTraffic / 1073741824, 2);
+    $RemainingVolume  = $min . "GB";
+    if ($min < 1) {
+        $RemainingVolume = $min * 1000 . "MB";
+    }
+    if (round($dataLimit / 1073741824, 2) - round($usedTraffic / 1073741824, 2)< 1  ) {
+        $RemainingVolume = "نامشخص";
+    }}
+
+
+//expire config
+$expire = $Data['expire'];
+$timestamp = $expire;
+$expirationDate = date('Y/m/d', $timestamp);
+$date_time_obj = new DateTime($expirationDate);
+if ($date_time_obj->format('Y/m/d') == '1970/01/01') {
+    $expirationDate = "نامحدود";
+}
+
+//remaining time
+$currentTime = time();
+$timeDiff = $expire - $currentTime;
+
+if ($timeDiff > 0) {
+    $day = floor($timeDiff / 86400) . " Day";
+} else {
+    $day = "نامحدود";
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>خروجی اطلاعات</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body>
+    <h3 class="title"><span><?php echo $username; ?></span> نمایش اطلاعات کاربر</h3>
+    <div class="box">
+        <div class="list username">
+            <div class="value">
+                <h4><?php echo $username; ?></h4>
+            </div>
+            <div class="user">
+                <h4>: نام کاربری</h4>
+            </div>
+        </div>
+        <div class="list status">
+            <div style="background-color:<?php echo  $color_active; ?>" class="value">
+                <h4><span><?php echo $text_status; ?></span></h4>
+            </div>
+            <div class="statusl">
+                <h4>: وضعیت سرویس </h4>
+            </div>
+        </div>
+        <div class="list total">
+            <div class="value">
+                <h4><?php echo $LastTraffic; ?></h4>
+            </div>
+            <div class="user">
+                <h4>:حجم کل سرویس</h4>
+            </div>
+        </div>
+        <div class="list usedTrafficGbs">
+            <div class="value">
+                <h4><?php echo $usedTrafficGb; ?></h4>
+            </div>
+            <div class="usedTrafficGb">
+                <h4>:حجم مصرف شده سرویس</h4>
+            </div>
+        </div>
+        <div class="list RemainingVolumes">
+            <div class="value">
+                <h4><?php echo $RemainingVolume; ?></h4>
+            </div>
+            <div class="RemainingVolume">
+                <h4>:حجم باقی مانده سرویس</h4>
+            </div>
+        </div>
+        <div class="list expirationDate">
+            <div class="value">
+                <h4><?php echo $expirationDate; ?></h4>
+            </div>
+            <div class="expirationDate">
+                <h4>:تاریخ پایان سرویس </h4>
+            </div>
+        </div>
+        <div class="list day">
+            <div class="value">
+                <h4><?php echo $day; ?></h4>
+            </div>
+            <div class="day">
+                <h4>:روزهای باقی مانده تا پایان سرویس </h4>
+            </div>
+        </div>
+    </div>
+    <div class="btndiv">
+    <a href="./index.html" class="btn">بازگشت به صفحه اصلی</a>
+
+    </div>
+</body>
+
+</html>
