2cdaa9c29a94c87506ac130d329bcf0bc71cff2a
MahdiGholipour <mahdigholipour@outlook.com>
2023-03-15 14:03:37 +0330

    Update config.php
    added Variable username
    added Variable password
    remove token Variable

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
 <?php
-$token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJtYWhkaTIwMDMiLCJhY2Nlc3MiOiJzdWRvIiwiZXhwIjoxNjc3MjcyMDE1fQ.lYBx64KS7QwPD8H_DWcA5o0F57UVmIWsqbfL1OoOKr8';//api توکن
-$ip_port = 'http://192.168.1.1:8880'; // آدرس آیپی همراه با پورت 
-?>
+$address_server = '192.168.1.1:8880/'; // آدرس آیپی یا دامنه همراه با پورت 
+$username= "admin"; //نام کاربری پنل ادمین
+$password= "admin"; // رمز عبور پنل ادمین
